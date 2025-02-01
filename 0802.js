5d449dc6692e1b7f442804e8992fbee6046afad6
midelq <pryst.oresta@gmail.com>
2025-02-01 18:40:24 +0600

    Update 0802.js
    ++

--- 0802.js
+++ 0802.js
@@ -22,3 +22,29 @@ loadScript("test1.js")
   })
   .catch(error => console.error(error));
 
+=======
+  
+      document.head.append(script); */
+      setTimeout(() => {
+        console.log(`emulate ${src} loading...`);
+        resolve(src);
+      }, 1000)
+    });
+  }
+  
+  loadScript("/article/promise-chaining/one.js")
+    .then( (src) => {
+      console.log(`emulate ${src} loaded!`);
+      loadScript("/article/promise-chaining/two.js")
+    })
+    .then( () => loadScript("/article/promise-chaining/three.js"))
+    .then( () => {
+      console.log('Hello learning async');
+    })
+    .catch(error => console.error(error));
+
+
+
+
+//sdadwdawdawdawdawd
+>>>>>>> 64740361d2348b04899f6eff9a84cc71b3c11271
